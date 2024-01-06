987f5528fa0100e69b583b697e7bd3f34ce15ea6
KR!$$H <48906449+KrisshRp@users.noreply.github.com>
2024-01-06 11:51:19 +0530

    init

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('#recentCase').slick({
+		dots: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: false,
+		nextArrow: false,
+	});
+});
+
